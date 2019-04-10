04232c6dd5a28ab6eeaba9af7baa2ae9892e4d0a
ayakkala1 <ayakkala@calpoly.edu>
2019-04-10 10:41:40 -0700

    add group by data analysis

--- lab1/schoolsearch.py
+++ lab1/schoolsearch.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from numpy import dtype
 import math
-
+import numpy as np
 def main():
     try:
         list_df = pd.read_csv("https://s3-us-west-1.amazonaws.com/csc365-spring2019/list.txt",
@@ -41,7 +41,10 @@ def main():
            "- E[nrollment] \n"
            "- I[nfo] \n"
            "- Q[uit] \n"
-         "Note: The text in arguments is optional while the rest is not. \n")
+           "- P[ivot]: <column> \n"
+         "Note: The text in arguments is optional while the rest is not. \n"
+         "Note: Pivot will always aggregate GPA by the column specified \n"
+         "      and return the aggregated variance and mean. \n")
     command = [[" "]]
     while command[0][0] != "Q":
         commands = input("Type in one of these commands! \n \n").upper()
@@ -81,6 +84,11 @@ def main():
                     grade_query(df, arguments[0], find_teach = True)
             else:
                 grade_query(df, arguments[0])
+        elif command[0][0] == "P":
+            if len(arguments) == 0:
+                print("Arguments not given")
+            else:
+                pivot_query(df,arguments[0])
         elif command[0][0] == "A":
             if len(arguments) == 0:
                 print("Arguments not given")
@@ -196,6 +204,17 @@ def lastname_query(df, lastname, bus = False):
                                                 "TFirstName"]]
         for row in filt_df.values:
             print(row)
+
+def pivot_query(df, group):
+    df_1 = df.copy()
+    df_1.columns = [x.upper() for x in df_1.columns]
+    if group == "GPA":
+        print("Can not aggregate GPA by GPA")
+    if group != "GPA" and group in df_1.columns:
+        print(df_1.pivot_table(index = group,
+                            columns = None,
+                            values = "GPA",
+                            aggfunc = [np.var,np.mean]))
 
 if __name__ == '__main__':
     main()   
